f227f3ef2dbb5fd711fbf46e9e7e30c6afe437aa
Jeff Jensen <jensen_jeffrey@bah.com>
2024-02-28 09:29:39 -0500

    syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ function retrieveValues {
   printLargeDelimeter
   if [ -d "$CHART_VALUES_DIR" ]; then
     echo "Locating additional values files"
-    CHART_VALUES_FILES=($(find "$CHART_VALUES_DIR" -type f -name "*.yaml" -o -name "*.yml"))
+    CHART_VALUES_FILES=$(find "$CHART_VALUES_DIR" -type f -name "*.yaml" -o -name "*.yml")
     echo "Located the following values files: ${CHART_VALUES_FILES[@]}"
   fi  
 }
